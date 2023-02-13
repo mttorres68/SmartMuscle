184a043be0760872351e8d2ec25de5c85ba7bebf
Alcides Neto <108681708+alcidesgoncalves@users.noreply.github.com>
2023-02-13 19:15:19 -0300

    Alteracao do botao

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,44 +1,35 @@
 import React, { useEffect } from "react";
-import { View, Text, StatusBar,TouchableOpacity } from "react-native";
+import { View, Text, StatusBar, TouchableOpacity } from "react-native";
 import styles from "./style";
-import {  onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import auth from "../../config/firebase.js";
 
-
-export default function Home({navigation}){
-
-    useEffect(() =>{        
-        onAuthStateChanged(auth, (user) => {
-            if (user) {                
-                navigation.navigate("PageTrainig", {idUser: user.id})                
-            }
-            });
-    }, []);
-    return (
-        <View style={styles.container}>
-            <StatusBar backgroundColor="#F65006"/>
-            <Text style={styles.title}>
-                SmartMuscle
-            </Text>
-            <View style={styles.button}>
-                <TouchableOpacity
-                    style={styles.buttonSingUp}
-                    onPress={() => navigation.navigate("SingUp")}
-                    
-                >
-                    <Text style={styles.textButtonSingUp}>
-                        Sing Up
-                    </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={styles.buttonLogin}
-                    onPress={() => navigation.navigate("Login")}
-                >
-                    <Text style={styles.textButtonLogin}>
-                        Login
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+export default function Home({ navigation }) {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigation.navigate("PageTrainig", { idUser: user.id });
+      }
+    });
+  }, []);
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#F65006" />
+      <Text style={styles.title}>SmartMuscle</Text>
+      <View style={styles.button}>
+        <TouchableOpacity
+          style={styles.buttonSingUp}
+          onPress={() => navigation.navigate("SingUp")}
+        >
+          <Text style={styles.textButtonSingUp}>Cadastra-se</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonLogin}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={styles.textButtonLogin}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
